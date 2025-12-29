91415e0889d49be7d11cdde0e824b62b3a4e6828
Katherine <145689231+Katherine2611@users.noreply.github.com>
2025-12-29 12:25:21 +0200

    Add initial Health Tracker application code

---
+++ ProgramCode.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HealthTrackerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Health Tracker: Water, Meditation & Books");
+        }
+    }
+}
